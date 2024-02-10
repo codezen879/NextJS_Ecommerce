7670f3eee5051acc29e32e2e8a7a98893b822438
BaldwinThe4 <sanchitcodecrafter@gmail.com>
2024-02-11 00:08:57 +0530

    pahse

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,20 @@
+import Image from 'next/image';
+import styles from './home.module.css'
 const Home = () => {
-  return <div>Hello World!</div>;
+  return <div className={styles.container}>
+    <div className={styles.textContainer}>
+      <h1>
+        Creative Thoughts Agency.
+      </h1>
+      <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+          blanditiis adipisci minima reiciendis a autem assumenda dolore.
+        </p>
+    </div>
+    <div className={styles.imgContainer}>
+      <Image src="/hero.gif" alt='gif' fill/>
+    </div>
+    </div>
 };
 
 export default Home;
