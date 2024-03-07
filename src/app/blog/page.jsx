95ce6c9f89af0380e0860d3b133b3c250e64c2c4
BaldwinThe4 <sanchitcodecrafter@gmail.com>
2024-03-07 18:37:18 +0530

    done

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,13 +1,23 @@
 import styles from "./blog.module.css"
 import PostCard from "@/components/postCard/postCard"
-const BlogPage=()=> {
+const getData=async()=>{
+const res= await fetch("https://jsonplaceholder.typicode.com/posts");
+if(!res.ok)
+{
+  throw new Error('Content cant be loaded');
+}
+return res.json();
+}
+
+const BlogPage=async()=> {
+  const data = await getData();
     return (
       <div>
-        <PostCard/>
-        <PostCard/>
-        <PostCard/>
-        <PostCard/>
-        <PostCard/>
+        <div className={styles.container}>
+        {data.map((data)=>
+        {return <PostCard data={data}/>}
+        )}
+        </div>
       </div>
     )
   }
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import styles from "./postCard.module.css"
-const PostCard=()=> {
+const PostCard=({data})=> {
 
     return (
       <div className={styles.container}>
@@ -8,12 +8,12 @@ const PostCard=()=> {
         <div className={styles.cardC}>
         <div className={styles.card}>
        <div className={styles.cardImage}>
-        <Image src="https://images.pexels.com/photos/2589453/pexels-photo-2589453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill ></Image>
+        <Image src="https://images.pexels.com/photos/20367978/pexels-photo-20367978/free-photo-of-landscape-of-rocky-snowcapped-mountains-and-a-forest-in-a-valley.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill ></Image>
         </div>
        </div>
-        <span>Content</span>
+        <span>{data.body}</span>
         </div>
-        <div className={styles.dte}><span>aoklkok</span></div>
+        <div className={styles.dte}><span>12/03/24</span></div>
       </div>
 
       </div>
